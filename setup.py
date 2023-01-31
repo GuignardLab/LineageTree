d6d57fea02dbb47550e7c02178ea542a4c0144fd
Léo Guignard <leo.guignard@univ-amu.fr>
2023-01-31 19:26:31 +0100

    remove python 2.7 dependecy

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
+with open(path.join(here, "README.md")) as f:
     long_description = f.read()
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
@@ -16,13 +16,14 @@ setup(
     version="1.0.0",
     description="Lineage tree structure for TGMM algorithm",
     long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/leoguignard/TGMMlibraries",
     author="Leo Guignard",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.8",
     ],
     packages=["LineageTree"],
     package_dir={"": "src"},
