11541793602af4197164b6581062d4e272562e12
BadPrograms <112393764+BadPrograms@users.noreply.github.com>
2023-08-03 16:52:51 +0200

    Update lineageTree.py

--- src/LineageTree/lineageTree.py
+++ src/LineageTree/lineageTree.py
@@ -1286,7 +1286,7 @@ class lineageTree(object):
 
         for spot in spots:
             unique_id = int(spot[1])
-            x, y, z = int(spot[5]),int(spot[6]),int(spot[7])
+            x, y, z = float(spot[5]),float(spot[6]),float(spot[7])
             t = int(spot[4])
             self.time_nodes.setdefault(t, set()).add(unique_id)
             self.nodes.add(unique_id)
