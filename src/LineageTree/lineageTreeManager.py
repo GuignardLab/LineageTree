9497d1a986817f6b9ccd45e19817f300eba33b36
BadPrograms <johnliaskas@gmail.com>
2024-04-28 10:39:30 +0300

    upd

--- src/LineageTree/lineageTreeManager.py
+++ src/LineageTree/lineageTreeManager.py
@@ -1,23 +1,41 @@
 import os
 import pickle as pkl
 from functools import partial
-from LineageTree import lineageTree
-from edist.uted import uted
+
 import numpy as np
+from edist.uted import uted
+
+from LineageTree import lineageTree
+
 
 class LineageTreeManager:
-
     def __init__(self):
         self.lineagetrees = {}
-        self.classification = {"Wt": {},"Ptb": {}}
+        self.classification = {"Wt": {}, "Ptb": {}}
         self.lineageTree_counter = 0
         self.registered = {}
 
     def get_next_tree(self):
-        self.lineageTree_counter+= 1
-        return self.lineageTree_counter-1
+        self.lineageTree_counter += 1
+        return self.lineageTree_counter - 1
 
-    def add(self,other_tree, name: str = None, type:str = None):
+    def add(self, other_tree:lineageTree, name: str = "", type: str = ""):
+        """Function that adds a new lineagetree object to the class.
+        Can be added either by .add or by using the + operator. If a name is
+        specified it will also add it as this specific name, otherwise it will
+        use the already existing name of the lineagetree. Lastly, it adds it to
+        the classification dict.
+
+        Args:
+            other_tree (lineageTree): Thelineagetree to be added.
+            name (str, optional): Then name of. Defaults to "".
+            type (str, optional): If the user need to add it to classification 
+                                    provide here the name of the class.
+                                    Defaults to "".
+
+        Returns:
+            _type_: _description_
+        """
         for tree in self.lineagetrees.values():
             if tree == other_tree:
                 return False
@@ -25,21 +43,26 @@ class LineageTreeManager:
             if name:
                 self.lineagetrees[name] = other_tree
             else:
-                name = other_tree.name#f"LineageTree  {self.get_next_tree()}"
+                name = other_tree.name
                 self.lineagetrees[name] = other_tree
-        if type in  ("Wt" ,"Ptb"):
-            self.classification[type] = {name:other_tree }
+        if type in ("Wt", "Ptb"):
+            self.classification[type] = {name: other_tree}
 
-    def __add__(self,other):
-        self.add(self,other)
+    def __add__(self, other):
+        self.add(other)
 
-    def classify_existing(self,key,type:str):
+    def classify_existing(self, key, type: str):
         if type in ("Wt", "Ptb"):
             self.classification[type] = {key: self.lineagetrees[key]}
         else:
             return False
 
     def write(self, fname: str):
+        """Saves the manager
+
+        Args:
+            fname (str): The path and name of the file that is to be saved.
+        """
         if os.path.splitext(fname)[-1] != ".ltM":
             fname = os.path.extsep.join((fname, "ltM"))
         with open(fname, "bw") as f:
@@ -47,7 +70,15 @@ class LineageTreeManager:
             f.close()
 
     def remove_embryo(self, key):
-        if key in self.lineagetrees.keys():
+        """Removes the embryo from the manager.
+
+        Args:
+            key (_type_): The name of the lineagetree to be removed
+
+        Raises:
+            Exception: If there is not such a lineagetree
+        """
+        if key in self.lineagetrees:
             del self.lineagetrees[key]
             if key in self.classification["Wt"]:
                 del self.classification["Wt"][key]
@@ -56,9 +87,8 @@ class LineageTreeManager:
         else:
             raise Exception("No such lineage")
 
-
     @classmethod
-    def load(cls,fname: str):
+    def load(cls, fname: str):
         """
         Loading a lineage tree Manager from a ".ltm" file.
 
@@ -73,31 +103,31 @@ class LineageTreeManager:
             f.close()
         return ltm
 
-    def edist_format(self,adj_dict: dict):
-            inv_adj = {vi: k for k, v in adj_dict.items() for vi in v}
-            roots = set(adj_dict).difference(inv_adj)
-            nid2list = {}
-            list2nid = {}
-            nodes = []
-            adj_list = []
-            curr_id = 0
-            for r in roots:
-                to_do = [r]
-                while to_do:
-                    curr = to_do.pop(0)
-                    nid2list[curr] = curr_id
-                    list2nid[curr_id] = curr
-                    nodes.append(curr_id)
-                    to_do = adj_dict.get(curr, []) + to_do
-                    curr_id += 1
-                adj_list = [
-                    [nid2list[d] for d in adj_dict.get(list2nid[_id], [])]
-                    for _id in nodes
-                ]
-            return nodes, adj_list, list2nid
-    
-    def cross_embryo_edit_distance(self, n1:int, embryo_1:str , end_time1:int, n2:int, embryo_2:str, end_time2:int, delta:callable = None, registration = None):
+    def cross_lineage_edit_distance(
+        self,
+        n1: int,
+        embryo_1: str,
+        end_time1: int,
+        n2: int,
+        embryo_2: str,
+        end_time2: int,
+        registration=None,
+    ):
+        """Compute the unordered tree edit distance from Zhang 1996 between the trees spawned
+        by two nodes `n1` from lineagetree1 and `n2` lineagetree2. The topology of the trees 
+        are compared and the matching cost is given by the function delta (see edist doc for 
+        more information).The distance is normed by the function norm that takes the two list
+        of nodes spawned by the trees `n1` and `n2`.
 
+        Args:
+            n1 (int): Node of the first Lineagetree
+            embryo_1 (str): The key/name of the first Lineagetree
+            end_time1 (int): End time of first Lineagetree
+            n2 (int): The key/name of the first Lineagetree
+            embryo_2 (str): Node of the second Lineagetree
+            end_time2 (int): End time of second lineagetree
+            registration (_type_, optional): _description_. Defaults to None.
+        """
         def delta(x, y, corres1, corres2, times1, times2):
             if x is None and y is None:
                 return 0
@@ -109,25 +139,31 @@ class LineageTreeManager:
             len_y = times2[corres2[y]]
             return np.abs(len_x - len_y)
 
+        def norm(times1, times2):
+            return max(sum(times1.values()), sum(times2.values()))
 
-        def norm(x,y):
-            return max(sum(times1.values()),
-                        sum(times2.values()))
-
-            # def norm(x,adj1,y,adj2,delta):
-            #     return max(uted(x,adj1,[],[],delta = delta),
-            #                uted(y,adj2,[],[],delta = delta))
-
-        simple_tree_1, times1 = self.lineagetrees[embryo_1].get_comp_tree(n1, end_time=end_time1)
-        simple_tree_2, times2 = self.lineagetrees[embryo_2].get_comp_tree(n2, end_time=end_time2)
-        nodes1, adj1, corres1 = self.lineagetrees[embryo_1]._edist_format(simple_tree_1)
-        nodes2, adj2, corres2 = self.lineagetrees[embryo_2]._edist_format(simple_tree_2)
+        simple_tree_1, times1 = self.lineagetrees[embryo_1].get_comp_tree(
+            n1, end_time=end_time1
+        )
+        simple_tree_2, times2 = self.lineagetrees[embryo_2].get_comp_tree(
+            n2, end_time=end_time2
+        )
+        nodes1, adj1, corres1 = self.lineagetrees[embryo_1]._edist_format(
+            simple_tree_1
+        )
+        nodes2, adj2, corres2 = self.lineagetrees[embryo_2]._edist_format(
+            simple_tree_2
+        )
         if len(nodes1) == len(nodes2) == 0:
             return 0
 
         delta_tmp = partial(
-            delta, corres1=corres1, times1= times1, corres2=corres2, times2 = times2
+            delta,
+            corres1=corres1,
+            times1=times1,
+            corres2=corres2,
+            times2=times2,
         )
         return uted(nodes1, adj1, nodes2, adj2, delta=delta_tmp) / norm(
-            n1,n2
-         )
+            times1, times2
+        )
