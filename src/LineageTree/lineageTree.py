909cb15f74394bc3eb477bb1cf8dd7e487c5cc24
ChrisMzz <christopher.mazzerbo@gmail.com>
2025-01-31 10:25:19 +0100

    added `depth` property to `lineageTree`

--- src/LineageTree/lineageTree.py
+++ src/LineageTree/lineageTree.py
@@ -390,6 +390,21 @@ class lineageTree(lineageTreeLoaders):
             self._time_resolution = 10
 
     @property
+    def depth(self):
+        if not hasattr(self, "_depth"):
+            self._depth = {}
+            for leaf in self.leaves:
+                self._depth[leaf] = 1
+                while leaf in self.predecessor:
+                    parent = self.predecessor[leaf][0]
+                    current_depth = self._depth.get(parent, 0)
+                    self._depth[parent] = max(self._depth[leaf]+1, current_depth)
+                    leaf = parent
+            for root in self.roots - set(self._depth):
+                self._depth[root] = 1
+        return self._depth
+
+    @property
     def roots(self):
         return set(self.nodes).difference(self.predecessor)
 
