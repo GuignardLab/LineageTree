f98ad5f51f410af4385e49d9e441d9f476663168
Léo Guignard <leo.guignard@univ-amu.fr>
2025-02-20 16:36:03 +0100

    fix: update GitHub link in documentation configuration

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ html_theme_options = {
     "icon_links": [
         {
             "name": "GitHub",
-            "url": "https://github.com/executablebooks/sphinx-book-theme",
+            "url": "https://github.com/Guignardlab/lineagetree",
             "icon": "fa-brands fa-github",
         },
     ],
