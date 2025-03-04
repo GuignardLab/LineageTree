105e52804c736361dbba97589284cc43438124e1
BadPrograms <johnliaskas@gmail.com>
2025-03-04 19:58:29 +0200

    update docs

--- src/LineageTree/utils.py
+++ src/LineageTree/utils.py
@@ -66,16 +66,23 @@ def write_csv_from_lT_to_lineaja(
             )
 
 
-def create_links_and_cycles(lT: lineageTree, roots=None):
-    """Generates a dictionary containing the links and the lengths of each branch.
-    Similar to simple tree, mainly used for tree manip app.
+def create_links_and_cycles(lT: lineageTree, roots=None) -> dict[str, dict]:
+    """Generates a dictionary containing all the edges (from start of lifetime to end not the intermediate timepoints)
+      of a subtree spawned by node/s and their duration
 
-    Args:
-        roots (Union[list,set,int]): The roots from which the tree will be generated.
 
-    Returns:
-        dict: A dictionary with keys "links" and "times" which contains the connections all cells and their branch
-        length.
+    Parameters
+    ----------
+    lT : lineageTree
+        The lineagetree that the user is working on
+    roots : _type_, optional
+        The root/s from which the tree/s will be generated, by default None
+
+    Returns
+    -------
+    dict[str,dict]
+        Returns a dictionary that contains 3 dictionaries the "links" ( contains all the edges) the "times" (contains all lifetime durations)
+        and "roots" (contains the roots.).
     """
     if roots is None:
         to_do = set(lT.roots)
@@ -105,18 +112,27 @@ def create_links_and_cycles(lT: lineageTree, roots=None):
 def hierarchical_pos(
     lnks_tms: dict, root, width=1000, vert_gap=2, xcenter=0, ycenter=0
 ) -> dict[int, list[int]]:
-    """Calculates the position of each node on te tree graph.
+    """Calculates the position of each node on the tree graph.
 
-    Args:
-        lnks_tms (dict): a dictionary created by create_links_and_cycles.
-        root (_type_): The id of the node, usually it exists inside lnks_tms dictionary, however you may use your own root.
-        width (int, optional): Max width, will not change the graph but the interacting with the graph takes this distance into account. Defaults to 1000.
-        vert_gap (int, optional): How far downwards each timepoint will go. Defaults to 2.
-        xcenter (int, optional): Where the root will be placed on the x axis. Defaults to 0.
-        ycenter (int, optional): Where the root will be placed on the y axis. Defaults to 0.
+    Parameters
+    ----------
+    lnks_tms : dict
+         a dictionary created by create_links_and_cycles.
+    root : _type_
+        The id of the node, usually it exists inside lnks_tms dictionary, however you may use your own root.
+    width : int, optional
+        Max width, will not change the graph but interacting with the graph takes this distance into account, by default 1000
+    vert_gap : int, optional
+        How far downwards each timepoint will go, by default 2
+    xcenter : int, optional
+        Where the root will be placed on the x axis, by default 0
+    ycenter : int, optional
+        Where the root will be placed on the y axis, by default 0
 
-    Returns:
-        dict[int, list[int]]: Provides a dictionary that contains the id of each node as keys and its position on the
+    Returns
+    -------
+    dict[int, list[int]]
+        Provides a dictionary that contains the id of each node as keys and its 2-d position on the
                                 tree graph as values.
     """
     to_do = [root]
