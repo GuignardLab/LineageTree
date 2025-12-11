77941ddd46ad5809c3052b8abde1f6f965449f17
BadPrograms <johnliaskas@gmail.com>
2025-12-11 10:16:18 +0100

    slightly faster

--- src/lineagetree/plot.py
+++ src/lineagetree/plot.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import copy
 import warnings
 from collections.abc import Iterable
 from typing import Literal, TYPE_CHECKING
@@ -27,14 +28,17 @@ def __plot_nodes(
     """
     Private method that plots the nodes of the tree.
     """
-    hier = {k: v for k, v in hier.items() if k not in leaves}
+    hier_no_leaves = copy.copy(hier)
+    for leaf in leaves:
+        hier_no_leaves.pop(key, None)
     if isinstance(color, dict):
-        color = [color.get(k, default_color) for k in hier]
+        color = [color.get(k, default_color) for k in hier_no_leaves]
     elif isinstance(color, str | list):
         color = [
-            color if node in selected_nodes else default_color for node in hier
+            color if node in selected_nodes else default_color
+            for node in hier_no_leaves
         ]
-    hier_pos = np.array(list(hier.values()))
+    hier_pos = np.array(list(hier_no_leaves.values()))
     ax.scatter(*hier_pos.T, s=size, zorder=10, color=color, **kwargs)
 
 
